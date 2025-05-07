3f03ee42b1f6e48f65f3afcd9063e702081c3ba9
Santiago Galo <51098556+ssgalo@users.noreply.github.com>
2025-05-07 11:02:06 -0300

    Ultimas correcciones de código y se sube la ultima version del notebook en colab

--- TP1-parte1/01_C/main.c
+++ TP1-parte1/01_C/main.c
@@ -13,137 +13,141 @@
 #define PROCESS_H 'H'
 #define PROCESS_I 'I'
 
-void error(char letter) 
+#define SLEEP_TIME_10 10
+#define SLEEP_TIME_20 20
+
+
+void error(char letter)
 {
     printf("Error de creacion en el proceso %c\n", letter);
     exit(EXIT_FAILURE);
 }
 
-void process(char letter) 
+void process(char letter)
 {
     pid_t pid = getpid();
     pid_t ppid = getppid();
     printf("Proceso %c (PID: %d) (PPID: %d)\n", letter, pid, ppid);
 
-    if (letter == PROCESS_A) 
+    if (letter == PROCESS_A)
     {
         pid_t b = fork();
-        if (b < 0) 
+        if (b < 0)
         {
             error(PROCESS_B);
-        } 
-        else if (b == 0) 
+        }
+        else if (b == 0)
         {
             process(PROCESS_B);
             exit(EXIT_SUCCESS);
         }
-        sleep(20);
+        sleep(SLEEP_TIME_20);
         wait(NULL);
     }
-    else if (letter == PROCESS_B) 
+    else if (letter == PROCESS_B)
     {
         pid_t c = fork();
-        if (c < 0) 
+        if (c < 0)
         {
             error(PROCESS_C);
-        } else if (c == 0) 
+        } else if (c == 0)
         {
             process(PROCESS_C);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
 
         pid_t d = fork();
-        if (d < 0) 
+        if (d < 0)
         {
             error(PROCESS_D);
-        } else if (d == 0) 
+        } else if (d == 0)
         {
             process(PROCESS_D);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
 
-        sleep(20);
+        sleep(SLEEP_TIME_20);
         wait(NULL);
         wait(NULL);
     }
-    else if (letter == PROCESS_D) 
+    else if (letter == PROCESS_D)
     {
         pid_t f = fork();
-        if (f < 0) 
+        if (f < 0)
         {
             error(PROCESS_F);
-        } else if (f == 0) 
+        } else if (f == 0)
         {
             process(PROCESS_F);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
 
         pid_t g = fork();
-        if (g < 0) 
+        if (g < 0)
         {
             error(PROCESS_F);
-        } else if (g == 0) 
+        } else if (g == 0)
         {
             process(PROCESS_F);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
 
-        sleep(20);
+        sleep(SLEEP_TIME_20);
         wait(NULL);
         wait(NULL);
     }
-    else if (letter == PROCESS_C) 
+    else if (letter == PROCESS_C)
     {
         pid_t e = fork();
-        if (e < 0) 
+        if (e < 0)
         {
             error(PROCESS_E);
-        } else if (e == 0) 
+        } else if (e == 0)
         {
             process(PROCESS_E);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
         wait(NULL);
     }
-    else if (letter == PROCESS_E) 
+    else if (letter == PROCESS_E)
     {
         pid_t h = fork();
-        if (h < 0) 
+        if (h < 0)
         {
             error(PROCESS_H);
-        } else if (h == 0) 
+        } else if (h == 0)
         {
             process(PROCESS_H);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
 
         pid_t i = fork();
-        if (i < 0) 
+        if (i < 0)
         {
             error(PROCESS_I);
-        } else if (i == 0) 
+        } else if (i == 0)
         {
             process(PROCESS_I);
-            sleep(10);
+            sleep(SLEEP_TIME_10);
             exit(EXIT_SUCCESS);
         }
 
         wait(NULL);
         wait(NULL);
     }
-    else 
+    else
     {
         sleep(10);
     }
 }
 
-int main() 
+int main()
 {
     process(PROCESS_A);
     return 0;
--- TP1-parte1/02_Python/main.py
+++ TP1-parte1/02_Python/main.py
@@ -3,30 +3,43 @@ import random
 import time
 import sys
 
+DICE_MIN_VALUE = 1
+DICE_MAX_VALUE = 6
+MIN_RANDOM_VALUE = 0.1
+MAX_RANDOM_VALUE = 0.3
+
 PLAYER = 5
 THROWS = 10
 
 def player(id):
     sys.stdout.write(f"Jugador {id} entra al juego.\n")
+    sys.stdout.flush()
     points = 0
     for i in range(THROWS):
-        dice = random.randint(1, 6)
+        dice = random.randint(DICE_MIN_VALUE, DICE_MAX_VALUE)
         points += dice
         sys.stdout.write(f"Jugador {id} - Lanzamiento {i+1}: {dice}\n")
-        time.sleep(random.uniform(0.1, 0.3))
+        sys.stdout.flush()
+        time.sleep(random.uniform(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE))
     sys.stdout.write(f"Jugador {id} finaliza con {points} puntos.\n")
+    sys.stdout.flush()
 
 def main():
-    children = []
+    children = [] #Creamos una lista para todos los procesos hijo
 
     for i in range(PLAYER):
+
+        #Creamos un nuevo proceso hijo para cada jugador
         pid = os.fork()
         if pid == 0:
+            # Es el proceso hijo
             player(i+1)
-            os._exit(os.EX_OK)  
+            os._exit(os.EX_OK)  # Finalizamos el proceso hijo
         else:
+            # Es el proceso padre
             children.append(pid)
 
+    # Mediante su pid, esperamos a que todos los hijos terminen
     for pid in children:
         os.waitpid(pid, 0)
 
